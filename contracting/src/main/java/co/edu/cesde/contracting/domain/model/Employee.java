beaf50803a7e6a2c1da5c03daff5c94535ffbd37
Thesimon391 <ruacuervos@gmail.com>
2026-04-18 17:15:13 -0500

    feature/HU-E4-02

--- contracting/src/main/java/co/edu/cesde/contracting/domain/model/Employee.java
+++ contracting/src/main/java/co/edu/cesde/contracting/domain/model/Employee.java
@@ -2,11 +2,14 @@ package co.edu.cesde.contracting.domain.model;
 
 import co.edu.cesde.contracting.application.dto.employeeActiveDTO;
 import co.edu.cesde.contracting.domain.enums.employedStatus;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
-
+@Getter
+@Setter
 public class Employee {
     private Long id;
     private Long applicantId;
@@ -20,13 +23,15 @@ public class Employee {
 
     private List<Contract> contracts;
 
-    public void active(){
+    public void active() {
         this.status = employedStatus.ACTIVE;
     }
-    public boolean isActive(){
+
+    public boolean isActive() {
         return this.status == employedStatus.ACTIVE || this.status == employedStatus.ON_PROBATION;
     }
-    public employeeActiveDTO toemployeeActiveDTO(){
+
+    public employeeActiveDTO toemployeeActiveDTO() {
         BigDecimal salary = contracts.stream()
                 .filter(Contract::isCurrent)
                 .map(Contract::getSalary)
@@ -35,90 +40,11 @@ public class Employee {
 
         return new employeeActiveDTO(
                 this.id,
-                this.firstName +" "+ this.lastName,
+                this.firstName + " " + this.lastName,
                 this.position,
                 this.department,
                 salary
         );
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public Long getApplicantId() {
-        return applicantId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public employedStatus getStatus() {
-        return status;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public List<Contract> getContracts() {
-        return contracts;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setApplicantId(Long applicantId) {
-        this.applicantId = applicantId;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public void setStatus(employedStatus status) {
-        this.status = status;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public void setContracts(List<Contract> contracts) {
-        this.contracts = contracts;
-    }
 }
